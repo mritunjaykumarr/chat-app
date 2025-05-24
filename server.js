511ee0dd13a776924ddea7cc568a01242e3d2f23
Mritunjay kumar <149924432+mritunjaykumarr@users.noreply.github.com>
2025-05-24 17:12:15 +0530

    Update server.js
    server updated

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const { v4: uuidv4 } = require('uuid');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000 
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
