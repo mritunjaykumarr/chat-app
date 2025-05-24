13eae739935c04dfcf08effce24cec1bfc79ecaf
Mritunjay kumar <149924432+mritunjaykumarr@users.noreply.github.com>
2025-05-24 17:23:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,34 +2,36 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const { v4: uuidv4 } = require('uuid');
-
+const path = require('path');
 const port = process.env.PORT || 4000;
 
-// Serve static files
 app.use(express.static('public'));
 
-// Routes
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/home.html');
+  res.sendFile(path.join(__dirname, '/public/home.html'));
 });
 
 app.get('/chat/:room', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-// Socket.io handling
-io.on('connection', socket => {
-  socket.on('join-room', roomId => {
-    socket.join(roomId);
+io.on("connection", socket => {
+  console.log('A user connected');
 
-    socket.on('chat-message', message => {
-      socket.to(roomId).emit('chat-message', message);
+  socket.on("join-room", roomId => {
+    socket.join(roomId);
+    console.log(`User joined room: ${roomId}`);
+
+    socket.on("chat-message", message => {
+      socket.to(roomId).emit("chat-message", message);
+    });
+
+    socket.on("typing", () => {
+      socket.to(roomId).emit("typing");
     });
   });
 });
 
-// Start server
 http.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
